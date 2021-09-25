1fb577d239957839555c0ddad6c6289178af6d83
Mubeen Yaqub <70191073+mobeenyaqub@users.noreply.github.com>
2021-09-25 21:56:56 +0500

    Update main.py

--- 11 - Black Jack/main.py
+++ 11 - Black Jack/main.py
@@ -1,115 +1,46 @@
-from art import logo
-from random import randint
+import art
 import os
+from random import randint
+from game_data import data
 
-user_cards = []
-user_sum = 0
-dealer_cards = []
-dealer_sum = 0
-count = 0
-play_again = 'y'
+print(art.logo)
 
-deck = {
-  2 : 2,
-  3 : 3,
-  4 : 4,
-  5 : 5,
-  6 : 6,
-  7 : 7,
-  8 : 8,
-  9 : 9,
-  10 : [10, "Jack", "King", "Queen"],
-  11 : "Ace"
-}
-
+score = 0
+winning = True
+choice = ''
 def cls():
   os.system('cls' if os.name=='nt' else 'clear')
 
-def random_card_assigner(app_card):
-  card_no = randint(2,11)
-  for card in deck:
-    if card_no == 10:
-      app_card.append(deck[card_no][randint(0,3)])
-      return app_card
-    elif card == card_no:
-      app_card.append(deck[card])
-      return app_card
+while winning:
+  random_num1 = randint(0,49)
+  random_num2 = randint(0,49)
+  while random_num1 == random_num2:
+    random_num2 = randint(0,49)
 
-def display_cards(count):
-  print(f"\n\nUser cards : {user_cards}")
-  if count == 0:
-    print(f"Dealer cards : {dealer_cards[0]}\n\n")
-    return 1
-  else:
-    print(f"Dealer cards : {dealer_cards}\n\n")
+  choice_A = data[random_num1]['follower_count']
+  choice_B = data[random_num2]['follower_count']
 
-def check_sum(card_sum = 0, cards = []):
-  card_sum = 0
-  for card in cards:
-    if card == "King" or card == "Queen" or card == "Jack":
-      card_sum += 10
-    elif card == "Ace":
-      if card_sum + 11 <= 21:
-        card_sum += 11
-      else: 
-        card_sum += 1
+  print(f"\n\nCompare A : {data[random_num1]['name']}, a {data[random_num1]['description']}, from {data[random_num1]['country']}")
+
+  print(art.vs)
+
+  print(f"Compare B : {data[random_num2]['name']}, a {data[random_num2]['description']}, from {data[random_num2]['country']}")
+
+  choice = input("\nWho has more followers? Type 'A' or 'B' : ").upper()
+
+  if choice == "A":
+    if choice_A > choice_B:
+      winning = True
+      score +=1
     else:
-      card_sum += card
-
-  return card_sum
-
-while play_again == "y":
+      winning = False
+  elif choice == "B":
+    if choice_B > choice_A:
+      winning = True
+      score +=1
+    else:
+      winning = False
 
   cls()
-  user_cards = []
-  dealer_cards = []
-  count = 0
-  print(logo, "\n\n")
 
-  for i in range(2):
-    user_cards = random_card_assigner(user_cards)
-    dealer_cards = random_card_assigner(dealer_cards)
-
-  count = display_cards(count)
-
-  choice = input("Press 'y' to add a card.\nPress 'n' to pass.\nEnter your choice : ")
-
-  while choice == "y":
-    user_cards = random_card_assigner(user_cards)
-    user_sum = check_sum(user_sum, user_cards)
-    print(f"\n{user_cards} User sum : {user_sum}\n")
-    dealer_sum = check_sum(dealer_sum, dealer_cards)
-    if user_sum > 21:
-      display_cards(count)
-      print(f"Dealer has won! Dealer points {dealer_sum}. User points {user_sum}.")
-      break
-    elif user_sum == 21:
-      display_cards(count)
-      print(f"User has won! User points {user_sum}. Dealer points {dealer_sum}.")
-      break
-
-    choice = input("Press 'y' to add a card.\nPress any key to pass.\nEnter your choice : ")  
-
-  else:
-    user_sum = check_sum(user_sum, user_cards)
-    dealer_cards = random_card_assigner(dealer_cards)
-    while dealer_sum < 17:
-      dealer_cards = random_card_assigner(dealer_cards)
-      dealer_sum = check_sum(dealer_sum, dealer_cards)
-    
-    if dealer_sum > 21:
-      display_cards(count)
-      print(f"\nUser has won. User points {user_sum}. Dealer points {dealer_sum}.")
-    elif dealer_sum == 21:
-      print(f"\nDealer has won! Dealer points {dealer_sum}. User sum {user_sum}.")
-    elif dealer_sum > user_sum:
-      display_cards(count)
-      print(f"\nDealer has won! Dealer points {dealer_sum}. User sum {user_sum}.")
-    elif dealer_sum == user_sum:
-      display_cards(count)
-      print(f"\nIt is a draw! User sum {user_sum}. Dealer points {dealer_sum}.")
-    else:
-      display_cards(count)
-      print(f"\nUser has won. User points {user_sum}. Dealer points {dealer_sum}.")
-
-  play_again = input("\n\n\nPress 'y' to play again.\nPress any key to exit.\nEnter your choice : ")
+print(f"\n\n\nYour score {score}")
