1ff56dca8c039131064a89162d0e8514e59afb48
Mubeen Yaqub <70191073+mobeenyaqub@users.noreply.github.com>
2024-01-28 12:36:01 +0500

    Exception Handling

--- odoo_marks_calculator.py
+++ odoo_marks_calculator.py
@@ -8,13 +8,20 @@ def run(playwright: Playwright):
     page = context.new_page()
     page.goto("https://odoo.cust.edu.pk/web/login")
     page.get_by_role("link", name="MS-SymbolLockup login With Microsoft").click()
-
-    page.get_by_label("Enter your email, phone, or Skype.").fill(input('Enter your email: '))
+    email = input('Enter your email: ').strip()
+    password = input('Enter your password: ').strip()
+    page.get_by_label("Enter your email, phone, or Skype.").fill(email)
     page.get_by_label("Enter your email, phone, or Skype.").press("Enter")
     time.sleep(2)
-    page.locator("#i0118").fill(input('Enter your password: '))
-    page.locator("#i0118").press("Enter")
-    page.get_by_role("button", name="No").click()
+    try:
+        page.locator("#i0118").fill(password)
+        page.locator("#i0118").press("Enter")
+        time.sleep(2)
+        page.get_by_role("button", name="No").click()
+        print('\nProcessing your grades...\n\n')
+    except:
+        print('\nSeems like an invalid email/password was entered!!\n\n')
+        return
     page.wait_for_selector('h6')
     teacher_names = [i.inner_text() for i in page.query_selector_all('h6')]
 
@@ -28,7 +35,7 @@ def run(playwright: Playwright):
         course_title = page.locator('#breadcrumbs li').nth(1).inner_text()
         ans = (f'{sum([actual_percentage[i] * obtained_percentage[i] / 100 for i in range(len(actual_percentage))])}'
                f' / {sum(actual_percentage)}')
-        
+
         page.get_by_role("link", name="home").click()
         time.sleep(2)
         print(f'{course_title}: {ans}')
